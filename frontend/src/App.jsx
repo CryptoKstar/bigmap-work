394a4099a548b4fa273e48be157e172542942ad5
CryptoKstar <88395668+CryptoKstar@users.noreply.github.com>
2022-02-22 09:56:38 -0800

    Update initial

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import Routes from './components/Routes/Routes';
-import 'antd/dist/antd.css';
-
-const App = () => (
-  <div className="App">
-    <Routes />
-  </div>
-);
-
-export default App;
+const App  = () => {
+  return(
+    <div>
+      <p>123</p>
+    </div>
+  )
+}
+export default App
--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,21 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
 import App from './App';
-import reducers from './state/reducers';
-
-const store = createStore(
-  reducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-);
 
 ReactDOM.render(
-  <Provider store={store}>
     <React.StrictMode>
       <App />
-    </React.StrictMode>
-  </Provider>,
+    </React.StrictMode>,
   document.getElementById('root'),
 );
